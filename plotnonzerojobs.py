7601ccf0db2cfc6fa06e49cacab54424f9a1e43e
sebastian <sebastian.burlacu@sickkids.ca>
2019-04-24 14:55:18 -0400

    plotting instead of counting

--- plotnonzerojobs.py
+++ plotnonzerojobs.py
@@ -5,16 +5,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 #%matplotlib inline
 matplotlib.style.use('ggplot')
-f=open("times/0420", "r")
+f=open("times/0423", "r")
 contents = f.read()
 jobs=[]
 for line in contents.splitlines():
     jobs.append(line.split()[0])
 
-#num_bins = 60
-#n, bins, patches = plt.hist(jobs, num_bins, facecolor='blue', alpha=0.5)
-#plt.show()
+num_bins = 1440
+n, bins, patches = plt.hist(jobs, num_bins, facecolor='blue', alpha=0.5)
+plt.show()
 
-from collections import Counter
-counted = Counter(jobs)
-print(counted)
+#from collections import Counter
+#counted = Counter(jobs)
+#print(counted)
